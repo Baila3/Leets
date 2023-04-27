5fc128642e02e31115f182cc130f60225ebda6c8
Baila3 <bailaly16@gmail.com>
2023-04-26 23:53:43 -0400

    romans completed

--- romans.js
+++ romans.js
@@ -4,56 +4,73 @@
  */
 var romanToInt = function(s) {
     const romans = s.split('')
-    let newA = 0;
+    let i = romans.length
     let integer = 0;
-    for (let i = 0; i < romans.length; i++) {
-        if (i === "I" && romans[romans.indexOf(i) + 1] != "V" || romans[romans.indexOf(i) + 1] != "X") {
+    for (const rom of romans) {
+        let real = romans.indexOf(rom) + 1
+        if (rom === "I" && (romans[romans.indexOf(rom) + 1] != "V" && romans[romans.indexOf(rom) + 1] != "X")) {
+            console.log(1)
             integer += 1
-            romans.shift()
-        } else if (i === "I" && romans[romans.indexOf(i) + 1] === "V") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom  === "I" && romans[romans.indexOf(rom)  + 1] === "V") {
+            console.log(4)
             integer += 4
-            romans.shift()
-            romans.shift()
-        } else if (i === "I" && romans[romans.indexOf(i) + 1] === "X") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  === "I" && romans[romans.indexOf(rom)  + 1] === "X") {
+            console.log(9)
             integer += 9
-            romans.shift()
-            romans.shift()
-        } else if (i === "V") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  === "V") {
+            console.log(5)
             integer += 5
-            romans.shift()
-        } else if (i === "X" && romans[romans.indexOf(i) + 1] != "L" || romans[romans.indexOf(i) + 1] != "C") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom  === "X" && (romans[romans.indexOf(rom)  + 1] != "L" && romans[romans.indexOf(rom)  + 1] != "C")) {
+            console.log(10)
             integer += 10
-            romans.shift()
-        } else if (i === "X" && romans[romans.indexOf(i) + 1] === "L") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom === "X" && romans[romans.indexOf(rom)  + 1] === "L") {
+            console.log(40)
             integer += 40
-            romans.shift()
-            romans.shift()
-        } else if (i === "X" && romans[romans.indexOf(i) + 1] === "C") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  === "X" && romans[romans.indexOf(rom)  + 1] === "C") {
+            console.log(90)
             integer += 90
-            romans.shift()
-            romans.shift()
-        } else if (i === "L") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  == "L") {
+            console.log(50)
             integer += 50
-            romans.shift()
-        } else if (i === "C" && romans[romans.indexOf(i) + 1] != "D" || romans[romans.indexOf(i) + 1] != "M") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom  === "C" && (romans[romans.indexOf(rom)  + 1] != "D" && romans[romans.indexOf(rom)  + 1] != "M")) {
+            console.log(100)
             integer += 100
-            romans.shift()
-        } else if (i === "C" && romans[romans.indexOf(i) + 1] != "D") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom  === "C" && romans[romans.indexOf(rom)  + 1] === "D") {
+            console.log(400)
             integer += 400
-            romans.shift()
-            romans.shift()
-        } else if (i === "C" && romans[romans.indexOf(i) + 1] === "M") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  === "C" && romans[romans.indexOf(rom)  + 1] === "M") {
+            console.log(900)
             integer += 900
-            romans.shift()
-            romans.shift()
-        } else if (i === "D") {
+            romans.splice(romans.indexOf(rom),1,0)
+            romans.splice(real,1,0)
+        } else if (rom  === "D") {
+            console.log(500)
             integer += 500
-            romans.shift()
-        } else if (i === "M") {
+            romans.splice(romans.indexOf(rom),1,0)
+        } else if (rom  === "M") {
+            console.log(1000)
             integer += 1000
-            romans.shift()
+            romans.splice(romans.indexOf(rom),1,0)
         }
-        console.log(romans)
+        // i--
+        // console.log(romans)
     }
     return integer
 };
+
+console.log("mile",romanToInt("L"))
