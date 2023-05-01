fa43f61c92faa915298fcd433a7f3d3a5fb1e98f
Baila3 <bailaly16@gmail.com>
2023-04-30 21:11:34 -0400

    2nd attempt at prefix

--- prefix.js
+++ prefix.js
@@ -2,7 +2,20 @@
  * @param {string[]} strs
  * @return {string}
  */
+const prefixes = [];
+const count = {};
 var longestCommonPrefix = function(strs) {
-  
-    
+  for (const word of strs) {
+    word.split('')
+    let pre = word.slice(0,1 + 1)
+    prefixes.push(pre)
+  }
+  prefixes.forEach(function(x) {count[x] = (count[x] || 0) + 1;})
+  if (Object.values(count).every( v => v < 2 )) {
+    return "";
+  }
+  console.log(Object.values(count).every( v => v < 2 ))
+  return _.max(Object.keys(count), o => count[o]);
 };
+
+console.log(longestCommonPrefix(["dog","racecar","doi","car"]))
