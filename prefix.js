deb1b7bdd7fdc30fa097b27db60e3e78caf4e681
Baila3 <bailaly16@gmail.com>
2023-05-04 23:57:03 -0400

    comments and performance test on  prefix

--- prefix.js
+++ prefix.js
@@ -9,18 +9,26 @@ var longestCommonPrefix = function(strs) {
   for (const word of strs) {
     let num = 0
     word.split('')
-    let x = 1;
+    let x = word.length;
+    // console.log(word.slice(0,1 + 3))
     while(x > 0) {
-      console.log("brot")
-      let startWith = strs.filter((str) => str.startsWith(word.slice(0,1 + num)))
-      if (startWith.length > 1) {
+
+      console.log(word)
+      let startWith = [];
+      // console.log("1",startWith)
+      // console.log(x)
+      startWith = strs.filter((str) => str.startsWith(word.slice(0,1 + num)))
+      // console.log("2",startWith)
+      if (startWith.length > 1 && num != word.length - 1) {
         x++
         prefixes.push(word.slice(0,1 + num))
         num++
+      } else {
+         x--
       }
       // console.log("boy",startWith)
+      console.log("me",prefixes)
     }
-    console.log("me",prefixes)
   //   if (word.length < 3) {
   //     word.split('')
   //     let pre = word.slice(0,1)
@@ -31,15 +39,19 @@ var longestCommonPrefix = function(strs) {
   //     prefixes.push(pre)
   //   }
   // }
-  // console.log(prefixes)
-  // prefixes.forEach(function(x) {count[x] = (count[x] || 0) + 1;})
-  // if (Object.values(count).every( v => v < 2 ) && prefixes.length > 1) {
-  //   return "";
-//  console.log("you",word.slice(0,1))
-//   let startWith = strs.filter((str) => str.startsWith(word.slice(0,1)))
-//   console.log(startWith)
-  }
+  console.log(prefixes)
+  
+  //  console.log("you",word.slice(0,1))
+  //   let startWith = strs.filter((str) => str.startsWith(word.slice(0,1)))
+  //   console.log(startWith)
+}
 
+prefixes.forEach(function(x) {count[x] = (count[x] || 0) + 1;})
+console.log(count)
+if (Object.values(count).every( v => v < 2 ) && prefixes.length > 1) {
+  return "";
+}
+return "there there"
   // return _.max(Object.keys(count), o => count[o]);
 };
 
