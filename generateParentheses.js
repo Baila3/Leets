ff0e0cc204c5b16c2e9da161d0943b4646b6934d
Baila3 <bailaly16@gmail.com>
2023-06-04 21:09:48 -0400

    create nested parent

--- generateParentheses.js
+++ generateParentheses.js
@@ -3,22 +3,22 @@
 var generateParenthesis = function(n) {
   let parent = "";
   let permutations = [];
-  const range = n * n
+  const range = n * n;
   for (let i = 0; i < n; i++) {
     parent += "()";
   }
-  permutations.push(parent)
-  let parentheses = parent.split("")
+  permutations.push(parent);
+  let parentheses = parent.split("");
   let x = 1;
-  for (let j = 0; j < n ; j++) {
+  for (let j = 0; j < n + 2 ; j++) {
     if (j == n - 1) {
-      parentheses.splice(0, 0, parentheses.splice(x - 2, 1)[0])
+      x = 0
     }
     parentheses.splice(x + 2, 0, parentheses.splice(x, 1)[0]);
-    permutations.push(parentheses.join(""))
-    x += 2
+    permutations.push(parentheses.join(""));
+    x += 2;
   }
   return permutations;
 };
 
-console.log(generateParenthesis(4))
+console.log(generateParenthesis(4));
