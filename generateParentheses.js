bbde4fe0ab3224103297d4857eb4d2a28a81854b
Baila3 <bailaly16@gmail.com>
2023-06-05 23:16:13 -0400

    halfway through

--- generateParentheses.js
+++ generateParentheses.js
@@ -1,24 +1,35 @@
 
 
 var generateParenthesis = function(n) {
-  let parent = "";
+  let son1 = "";
+  let son2 = "";
   let permutations = [];
   const range = n * n;
   for (let i = 0; i < n; i++) {
-    parent += "()";
+    son1 += "(";
+    son2 += ")";
   }
+  const parent = son1 + son2
   permutations.push(parent);
   let parentheses = parent.split("");
-  let x = 1;
-  for (let j = 0; j < n + 2 ; j++) {
-    if (j == n - 1) {
-      x = 0
-    }
-    parentheses.splice(x + 2, 0, parentheses.splice(x, 1)[0]);
+  let x = n;
+  let v = n - 1
+  for (let j = 0; j < n -1   ; j++) {
+    parentheses.splice(x - v, 0, parentheses.splice(x, 1)[0]);
     permutations.push(parentheses.join(""));
-    x += 2;
+    x += 1;
+    v -= 1
   }
+  x = 0
+  v = n - 1
+  // for (let d = 0; d < n - 1 ; d++) {
+  //   parentheses.splice(x + v, 0, parentheses.splice(x, 1)[0]);
+  //   permutations.push(parentheses.join(""));
+  //   x += 1;
+  //   v -= 1
+  // }
+  
   return permutations;
 };
 
-console.log(generateParenthesis(4));
+console.log(generateParenthesis(1));
