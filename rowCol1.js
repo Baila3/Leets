9102dbc78d9f9a3bc542072b5fa4435ee2bdefc0
Baila3 <bailaly16@gmail.com>
2023-06-18 21:59:06 -0400

    fixing output limit problem

--- rowCol1.js
+++ rowCol1.js
@@ -1,26 +1,25 @@
 const equalPairs = function(grid) {
-  const big = [];
+  let big = [];
   let num = 0;
-  let ans = 0;
   const nGrid = grid.map(JSON.stringify)
   for (let i = 0; i < grid.length; i++) {
+    if (grid.length != grid[i].length) {
+      return 0
+    }
     let boy = [];
     for (let j = 0; j < grid[i].length; j ++) {
       boy.push(grid[j][num]);
     }
-   
-    big.push(boy);
     if (nGrid.includes(JSON.stringify(boy))) {
-      let index = nGrid.indexOf(JSON.stringify(boy))
-      nGrid.splice(index, 1)
-      ans++;
+      let newGrid = nGrid.filter(list => list == JSON.stringify(boy))
+      big.push(...newGrid)
+      console.log("m",newGrid)
     }
+    console.log(big)
     num++;
   }
-  console.log(nGrid)
-
-  console.log(big)
+  let ans = big.length
   return ans;
 };
 
-console.log(equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
+console.log(equalPairs([[3,2,1],[1,7,6],[2,7,7]]))
