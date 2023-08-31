a84870533472e1e61523d8a2ed536da370c1a2c7
Baila3 <bailaly16@gmail.com>
2023-08-30 21:50:55 -0400

    steps try

--- plusOne.js
+++ plusOne.js
@@ -1,9 +1,38 @@
-var plusOne = function(digits) {
-    let digo = String(digits.join(""))
-    const newNum = BigInt(digo)
-    console.log(newNum + 1)
-    const newArr = Array.from(String(newNum), Number)
-    return newArr
+var climbStairs = function(n) {
+    
+    
 };
 
-console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+1 1 1 1
+2 1 1
+1 2 1
+1 1 2
+2 2
+
+1 1 1 1 1
+2 1 1 1
+1 2 1 1
+1 1 2 1
+1 1 1 2
+1 2 2
+2 1 2
+2 2 1
+
+
+1 1 1 1 1 1
+2 1 1 1 1
+1 2 1 1 1
+1 1 2 1 1
+1 1 1 2 1
+1 1 1 1 2
+2 2 1 1
+2 1 2 1
+2 1 1 2
+1 2 1 2
+1 1 2 2
+1 2 2 1
+2 2 2
+
+
+
+
